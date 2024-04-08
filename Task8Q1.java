2dab4d6a6d37038ee47fd60d2b362b6534531a32
pash7464 <155511621+pash7464@users.noreply.github.com>
2024-04-08 14:53:51 +0530

    Add files via upload

---
+++ Task8Q1.java
@@ -0,0 +1,20 @@
+package javapackage;
+
+public class Task8Q1 {
+
+	public static void main(String[] args) {
+		
+		int a = 40;
+		int b = 30;
+		int c = 20;
+		int d = 10;
+		
+		if(a+b > c+d) {
+			System.out.println("Sum of a and b is greater than sum of c and d");	
+		}
+		else {
+			System.out.println("Sum of a and b is smaller than sum of c and d");
+		}
+	}
+
+}
