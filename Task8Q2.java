2d494a45a5b607889821523f36818ef787fdfe9b
pash7464 <155511621+pash7464@users.noreply.github.com>
2024-04-08 15:15:33 +0530

    Add files via upload

---
+++ Task8Q2.java
@@ -0,0 +1,15 @@
+package javapackage;
+
+public class Task8Q2 {
+	//Check even number
+	public static void main(String[] args) {
+		
+		int z=5;
+		if (z % 2 == 0) {
+			System.out.println("The number is even");
+		}
+		else {
+			System.out.println("THe number is odd");
+		}
+	}
+}
