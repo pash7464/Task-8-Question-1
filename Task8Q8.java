a2957b407942a08afb2212206296df3f26367355
pash7464 <155511621+pash7464@users.noreply.github.com>
2024-04-10 12:48:54 +0530

    Add files via upload

---
+++ Task8Q8.java
@@ -0,0 +1,14 @@
+package javapackage;
+
+public class Task8Q8 {
+
+	public static void main(String[] args) {
+		
+		int n = 10;
+	    for (int i = 1; i <= n; ++i) {
+	      System.out.println("Welcome to guvi");
+	    }
+		
+	}
+
+}
